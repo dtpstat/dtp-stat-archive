2b93a44db5291a8ca4730794028e0ddf8165e3df
Ivlev Denis <di-erz@yandex.ru>
2019-02-10 13:12:08 +0300

    Add aggregation

--- api/v1/views.py
+++ api/v1/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
+
+from django.db.models import Q, Sum
 
 from dtpmapapp.models import (
     MVC,
@@ -27,8 +30,40 @@ from .serializers import (
 
 
 class MVCViewSet(viewsets.ModelViewSet):
-    queryset = MVC.objects.all()
+    queryset = MVC.objects.select_related("participant_type").all()
     serializer_class = MVCSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(
+            self.get_queryset().only("id", "datetime", "participant_type", "lng", "lat")
+        )
+        serializer = self.get_serializer(queryset, many=True)
+        aggregate = queryset.aggregate(
+            dead=Sum("dead"),
+            dead_auto=Sum("dead", filter=Q(participant_type__name="auto")),
+            dead_bicycle=Sum("dead", filter=Q(participant_type__name="bicycle")),
+            dead_pedestrian=Sum("dead", filter=Q(participant_type__name="pedestrian")),
+            injured=Sum("injured"),
+            injured_auto=Sum("injured", filter=Q(participant_type__name="auto")),
+            injured_bicycle=Sum("injured", filter=Q(participant_type__name="bicycle")),
+            injured_pedestrian=Sum(
+                "injured", filter=Q(participant_type__name="pedestrian")
+            ),
+        )
+
+        data = {
+            "count": queryset.count(),
+            "result": serializer.data,
+            "dead": aggregate["dead"],
+            "deadAuto": aggregate["dead_auto"],
+            "deadBicycle": aggregate["dead_bicycle"],
+            "deadPedestrian": aggregate["dead_pedestrian"],
+            "injured": aggregate["injured"],
+            "injuredAuto": aggregate["injured_auto"],
+            "injuredBicycle": aggregate["injured_bicycle"],
+            "injuredPedestrian": aggregate["injured_pedestrian"],
+        }
+        return Response(data)
 
 
 class CarViewSet(viewsets.ModelViewSet):
